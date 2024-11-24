632b7764710cc5314fd655d56f0e5b6ef3ffd7b2
Gabriel <gabrielgk2003@gmail.com>
2024-11-24 18:38:43 +0100

    Configurar ProcessBuilder para ejecutar el comando

--- src/ud1_practica1_opcionb/UD1_Practica1_OpcionB.java
+++ src/ud1_practica1_opcionb/UD1_Practica1_OpcionB.java
@@ -3,7 +3,17 @@ package ud1_practica1_opcionb;
 public class UD1_Practica1_OpcionB {
 
     public static void main(String[] args) {
-        // Punto de inicio del programa
-    }
+        try {
+            // Crear el proceso con ProcessBuilder
+            ProcessBuilder processBuilder = new ProcessBuilder("ip", "a");
+            processBuilder.redirectErrorStream(true);
 
+            // Iniciar el proceso
+            Process process = processBuilder.start();
+
+            System.out.println("Proceso iniciado");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
